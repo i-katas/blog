68957b8eebad9722fb1f68b04566da7c63ba68fc
i-katas <feexonj@gmail.com>
2020-02-20 23:34:50 +0800

    fix node dependencies

--- web/test/BlogList.test.jsx
+++ web/test/BlogList.test.jsx
@@ -4,7 +4,7 @@ describe('BlogList', () => {
     it('display empty blogs', function () {
         let blogs = shallow(<BlogList items={[]}/>);
 
-        expect(blogs.find('#blogs')).toHaveTagName("ul");
+        expect(blogs.find('#blogs')).toHaveDisplayName("ul");
         expect(blogs.find('#blogs').children().exists()).toBeFalsy()
     });
 });
