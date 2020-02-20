75e53631109da116e8e1ead04b34bdea4ebae3f5
i-katas <feexonj@gmail.com>
2020-02-20 23:31:39 +0800

    fix test assertion

--- web/test/BlogList.test.jsx
+++ web/test/BlogList.test.jsx
@@ -4,7 +4,7 @@ describe('BlogList', () => {
     it('display empty blogs', function () {
         let blogs = shallow(<BlogList items={[]}/>);
 
-        expect(blogs.find('#blogs').exists()).toBe(true)
-        expect(blogs.find('#blogs').children().exists()).toBe(false)
+        expect(blogs.find('#blogs')).toHaveTagName("ul");
+        expect(blogs.find('#blogs').children().exists()).toBeFalsy()
     });
 });
