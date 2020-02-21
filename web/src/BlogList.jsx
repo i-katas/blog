bce45dd5d483945460d2d425c5c8467428bb0b24
i-katas <feexonj@gmail.com>
2020-02-21 16:42:33 +0800

    display no blogs if there is no blogs published by blogger

--- web/src/BlogList.jsx
+++ web/src/BlogList.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export default class extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {loaded: false};
+    }
+
+    componentDidMount() {
+        this.props.items.then(() => this.setState({loaded: true}))
+    }
+
     render() {
-        return <ul id='blogs'/>;
+        let {loaded} = this.state;
+
+        return (
+            <ul id='blogs'>{loaded && <li className='empty'>No blogs</li>}</ul>
+        )
     }
 }
--- web/test/BlogList.test.jsx
+++ web/test/BlogList.test.jsx
@@ -1,10 +1,21 @@
 import BlogList from 'BlogList'
 
-describe('BlogList', () => {
-    it('display empty blogs', function () {
-        let blogs = shallow(<BlogList items={[]}/>);
+function flushPromises() {
+    return new Promise(resolve => setImmediate(resolve));
+}
 
-        expect(blogs.find('#blogs')).toHaveDisplayName("ul");
-        expect(blogs.find('#blogs').children().exists()).toBeFalsy()
+describe('BlogList', () => {
+    it('display empty blogs', () => {
+        let blogs = mount(<BlogList items={Promise.resolve({items: []})}/>);
+
+        return flushPromises().then(() => {
+            expect(blogs.find('#blogs')).toHaveDisplayName("ul");
+            expect(blogs.find('#blogs').find("li.empty").exists()).toBe(false);
+            blogs.update();
+
+            expect(blogs.find('#blogs')).toHaveDisplayName("ul");
+            expect(blogs.find('#blogs').find("li.empty").exists()).toBe(true);
+            expect(blogs.find('#blogs').find("li.empty")).toHaveText("No blogs");
+        });
     });
 });
